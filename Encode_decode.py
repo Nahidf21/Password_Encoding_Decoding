dd90c5f4ddc553bd81a6f3c4a5d94e79e611ebeb
Nahid Ferdous <nahid.mferdous@gmail.com>
2022-03-18 23:23:03 -0500

    Find Some error
    IndexError: list index out of range.
    need to modify the code. sometimes it shows error.

--- Encode_decode.py
+++ Encode_decode.py
@@ -4,8 +4,6 @@ alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r
 direction=input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text=input("Type your message:\n").lower()
 shift=int(input("Type the shift number: \n"))
-
-
 def encrypt(plain_text, shift_amount):
     cipher_text=""
     for letter in plain_text:
@@ -18,7 +16,6 @@ def encrypt(plain_text, shift_amount):
         new_letter=alphabet[new_position]
         cipher_text+=new_letter
     print(f"The encodeed text is {cipher_text}")
-
 def decrypt(plain_text, shift_amount):
     cipher_text=""
     for letter in plain_text:
